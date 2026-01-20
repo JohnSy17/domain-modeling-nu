a99234aa721f86622a2604bf2d7836edf25de10d
JohnSy17 <john.syme@kellogg.northwestern.edu>
2026-01-20 17:33:00 +0000

    New CRM and SQL table

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,50 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industry;
+DROP TABLE IF EXISTS industries;
+
 -- CREATE TABLES
+
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  status TEXT,
+  email TEXT,
+  phone_number TEXT
+);
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  salesperson_id INTEGER,
+  company_id INTEGER
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  activity_type TEXT,
+  date TEXT,
+  notes TEXT,
+  contact_id INTEGER
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_name TEXT,
+  industry_id INTEGER
+);
+
+CREATE TABLE industries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_id INTEGER,
+  industry_id INTEGER
+);
